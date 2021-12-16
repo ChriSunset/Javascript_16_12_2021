f61400e6b4dce410a434650b3a905de3906cd7ca
Picot <c.picot.iris@gmail.com>
2021-12-16 15:21:39 +0100

    Js_16_12_2021

---
+++ js/variable.js
@@ -0,0 +1,11 @@
+"use strict";
+// commentaire 1
+/* 
+
+commentaire 2
+
+*/
+// KamelCase on commence en minuscule et a chaque nouveau mots on met une majuscule
+// Une variable c'est un espace dans lequel on stock des choses : c'est une boite que l'on nomme
+var unTexte = "Voici un texte"; // ce qui est à droite donne sa valeur de ce qui est à gauche/
+console.log(unTexte);
